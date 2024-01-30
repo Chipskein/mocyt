e3b96349c0e16c9694d2c5697fe64e0fb9246186
Chipskein <abfn0905@gmail.com>
2024-01-30 13:16:07 -0300

    iniciando a mexer na ui

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module chipskein/yta-cli
 go 1.21.6
 
 require (
+	github.com/gizak/termui/v3 v3.1.0
+	golang.org/x/oauth2 v0.16.0
+	google.golang.org/api v0.159.0
+)
+
+require (
 	cloud.google.com/go/compute v1.23.3 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
@@ -14,13 +20,9 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
-	github.com/klauspost/compress v1.10.4 // indirect
-	github.com/lithdew/bytesutil v0.0.0-20200409052507-d98389230a59 // indirect
-	github.com/lithdew/nicehttp v0.0.0-20200422123956-0d3d3dd9b482 // indirect
-	github.com/lithdew/youtube v0.0.0-20200524132213-a1fd5d688f8a // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.12.0 // indirect
-	github.com/valyala/fastjson v1.5.0 // indirect
+	github.com/mattn/go-runewidth v0.0.2 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.47.0 // indirect
 	go.opentelemetry.io/otel v1.22.0 // indirect
@@ -28,11 +30,8 @@ require (
 	go.opentelemetry.io/otel/trace v1.22.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/oauth2 v0.16.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/api v0.159.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240125205218-1f4bbc51befe // indirect
 	google.golang.org/grpc v1.61.0 // indirect
---
+++ internals/ui/ui.go
@@ -0,0 +1,256 @@
+package ui
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	tui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
+
+var wg sync.WaitGroup
+
+type TUI struct {
+	grid             *tui.Grid
+	ticker           *<-chan time.Time
+	tickerProgresBar *<-chan time.Time
+	uiEvents         <-chan tui.Event
+	progressBar      *widgets.Gauge
+	volumeBar        *widgets.Gauge
+	filelist         *widgets.List
+	p                *widgets.Paragraph
+	//repo             *repositories.LocalRepository
+	//player           *player.PlayerController
+}
+
+func (t *TUI) RenderFileList() {
+	filelist := widgets.NewList()
+	filelist.Rows = []string{} //t.repo.ListFiles()
+	filelist.Title = ""        //t.repo.CURRENT_DIRECTORY
+	filelist.TitleStyle.Fg = tui.ColorWhite
+	filelist.SelectedRowStyle.Fg = tui.ColorBlack
+	filelist.SelectedRowStyle.Bg = tui.ColorWhite
+	filelist.TextStyle.Fg = tui.ColorWhite
+	t.filelist = filelist
+}
+func (t *TUI) RenderVolumeMixer() {
+	volumeBar := widgets.NewGauge()
+	volumeBar.TitleStyle.Fg = tui.ColorWhite
+	volumeBar.Percent = 100
+	volumeBar.Label = fmt.Sprintf("Volume %d%%", volumeBar.Percent)
+	volumeBar.BarColor = tui.ColorWhite
+	volumeBar.LabelStyle = tui.NewStyle(tui.ColorWhite)
+	t.volumeBar = volumeBar
+}
+func (t *TUI) RenderProgressBar() {
+
+	processBar := widgets.NewGauge()
+	processBar.TitleStyle.Fg = tui.ColorWhite
+	processBar.Percent = 0
+	processBar.Label = " "
+	processBar.BarColor = tui.ColorWhite
+	processBar.LabelStyle = tui.NewStyle(tui.ColorWhite)
+	t.progressBar = processBar
+}
+func (t *TUI) RenderSongInfo() {
+	p := widgets.NewParagraph()
+	p.Text = fmt.Sprintf("Time:%s  Duration:%s", "0s", "0s")
+	t.p = p
+}
+func (t *TUI) SetupGrid() {
+	grid := tui.NewGrid()
+	termWidth, termHeight := tui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
+
+	grid.Set(
+		tui.NewRow(1.6/2,
+			tui.NewCol(2/2,
+				t.filelist),
+		),
+		tui.NewRow(0.2/2,
+			tui.NewCol(1.5/2, t.p),
+			tui.NewCol(0.5/2, t.volumeBar),
+		),
+		tui.NewRow(0.18/2, t.progressBar),
+	)
+	t.grid = grid
+	t.RenderUI()
+}
+func (t *TUI) HandleTUIEvents() {
+	for {
+		select {
+		case e := <-t.uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			case "<Enter>":
+				t.HandleSelectedFile(t.filelist.Rows[t.filelist.SelectedRow])
+			case "h":
+				//t.repo.ShowHiddenFiles = !t.repo.ShowHiddenFiles
+				//t.filelist.Rows = t.repo.ListFiles()
+				t.RenderUI()
+			case "<Down>", "j":
+				t.filelist.ScrollDown()
+			case "<Up>", "k":
+				t.filelist.ScrollUp()
+			case "<End>":
+				t.filelist.ScrollBottom()
+			case "<Home>":
+				t.filelist.ScrollTop()
+			case "<PageDown>":
+				t.filelist.ScrollHalfPageDown()
+			case "<PageUp>":
+				t.filelist.ScrollHalfPageUp()
+			case "<Space>":
+				/*
+					if t.player != nil {
+						go t.player.PauseOrResume()
+						if !t.player.Ctrl.Paused {
+							t.progressBar.Title = "|| Paused"
+						} else {
+							t.progressBar.Title = "|> Playing"
+						}
+						t.RenderUI()
+					}
+				*/
+			case ",":
+				/*
+					if t.player != nil {
+						if t.volumeBar.Percent > 0 {
+							t.volumeBar.Percent--
+							go t.player.VolumeDown()
+							wg.Add(1)
+							wg.Done()
+						} else {
+							t.volumeBar.Percent = 0
+						}
+						t.volumeBar.Label = fmt.Sprintf("Volume %d%%", t.volumeBar.Percent)
+						t.RenderUI()
+					}
+				*/
+			case ".":
+				/*
+					if t.player != nil {
+						if t.volumeBar.Percent < 100 {
+							t.volumeBar.Percent++
+							go t.player.VolumeUp()
+							wg.Add(1)
+							wg.Done()
+						} else {
+							t.volumeBar.Percent = 100
+						}
+						t.volumeBar.Label = fmt.Sprintf("Volume %d%%", t.volumeBar.Percent)
+						t.RenderUI()
+					}
+				*/
+			case "m":
+				/*
+					if t.player != nil {
+						go t.player.Mute()
+						wg.Add(1)
+						wg.Done()
+						if !t.player.Volume.Silent {
+							t.volumeBar.Label = "MUTED"
+						} else {
+							t.volumeBar.Label = fmt.Sprintf("Volume %d%%", t.volumeBar.Percent)
+						}
+						t.RenderUI()
+					}
+				*/
+			case "<Resize>":
+				payload := e.Payload.(tui.Resize)
+				t.grid.SetRect(0, 0, payload.Width, payload.Height)
+				tui.Clear()
+				t.RenderUI()
+			}
+
+		case <-*t.ticker:
+			t.RenderUI()
+		case <-*t.tickerProgresBar:
+			/*
+				if t.player != nil && !t.player.Ctrl.Paused && t.progressBar.Percent < 100 {
+					t.progressBar.Percent++
+				}
+			*/
+			t.RenderUI()
+		}
+		/*
+			if t.player != nil && !t.player.Ctrl.Paused {
+				var now = t.player.Samplerate.D(t.player.Streamer.Position()).Round(time.Second)
+				var duration = t.player.Samplerate.D(t.player.Streamer.Len()).Round(time.Second)
+				t.p.Text = fmt.Sprintf("Time:%s  Duration:%s", now, duration)
+				t.RenderUI()
+			}
+		*/
+	}
+}
+func (t *TUI) HandleSelectedFile(filename string) {
+	var persistInfo = false
+	if filename == "../" {
+		//parentPath := path.Dir(t.repo.CURRENT_DIRECTORY)
+		//t.repo.CURRENT_DIRECTORY = "."
+		t.filelist.Rows = []string{} //t.repo.ListFiles()
+		t.filelist.Title = ""        //t.repo.CURRENT_DIRECTORY
+		return
+	}
+	//var file = t.repo.Files[filename]
+	//if file.IsADirectory {
+	//	t.repo.CURRENT_DIRECTORY = file.FullPath
+	//	t.filelist.Rows = t.repo.ListFiles()
+	//		t.filelist.Title = t.repo.CURRENT_DIRECTORY
+	//		return
+	//}
+	//if t.player != nil {
+	//	player.OLD_PERCENT = t.volumeBar.Percent
+	//	player.OLD_VOLUME = t.player.Volume.Volume
+	//	persistInfo = true
+	//	t.player.Stop()
+	//}
+	//var f = repositories.ReadFile(file.FullPath)
+	//streamer, format, _ := decoder.Decode(f, file.Extension)
+
+	//t.player = player.InitPlayer(format.SampleRate, streamer, f)
+	//go t.player.Play()
+	t.progressBar.Percent = 0
+	t.progressBar.Title = "|> Playing"
+	t.progressBar.Label = filename
+	//var now = t.player.Samplerate.D(t.player.Streamer.Position()).Round(time.Second)
+	//var duration = t.player.Samplerate.D(t.player.Streamer.Len()).Round(time.Second)
+	//t.p.Text = fmt.Sprintf("Time:%s  Duration:%s", now, duration)
+
+	if persistInfo {
+		//t.player.Volume.Volume = player.OLD_VOLUME
+		//t.volumeBar.Percent = player.OLD_PERCENT
+	}
+	//t.tickerProgresBar = &time.NewTicker(duration / 100).C
+
+}
+func (t *TUI) RenderUI() {
+	tui.Render(t.grid)
+}
+
+func StartUI(CURRENT_DIRECTORY string, DEFAULT_DIRECTORY string, ShowHiddenFiles bool) {
+	if err := tui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer tui.Close()
+
+	var t = &TUI{}
+	//t.repo = &repositories.LocalRepository{CURRENT_DIRECTORY: CURRENT_DIRECTORY, DEFAULT_DIRECTORY: DEFAULT_DIRECTORY, ShowHiddenFiles: ShowHiddenFiles}
+
+	go t.RenderFileList()
+	go t.RenderVolumeMixer()
+	go t.RenderProgressBar()
+	go t.RenderSongInfo()
+	wg.Add(4)
+	wg.Done()
+	time.Sleep(time.Millisecond * 500)
+	t.SetupGrid()
+	t.uiEvents = tui.PollEvents()
+	t.ticker = &time.NewTicker(time.Microsecond).C
+	t.tickerProgresBar = &time.NewTicker(time.Second).C
+	t.HandleTUIEvents()
+
+}
